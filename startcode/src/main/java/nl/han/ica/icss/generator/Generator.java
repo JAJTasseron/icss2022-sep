cb6def56d6592d27156107f9ffd26771475ab012
Jur Tasseron <JAJ.Tasseron@student.han.nl>
2025-10-21 07:56:11 +0200

    Werkende generator

--- startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
+++ startcode/src/main/java/nl/han/ica/icss/generator/Generator.java
@@ -1,10 +1,8 @@
 package nl.han.ica.icss.generator;
 
 
-import nl.han.ica.icss.ast.AST;
-import nl.han.ica.icss.ast.ASTNode;
-import nl.han.ica.icss.ast.Stylerule;
-import nl.han.ica.icss.ast.Stylesheet;
+import nl.han.ica.icss.ast.*;
+import nl.han.ica.icss.ast.literals.*;
 
 public class Generator {
 
@@ -15,18 +13,42 @@ public class Generator {
 	// Onderdelen
 	private String generateStylesheet(Stylesheet root) {
 		if(root.getChildren().isEmpty()) return "";
-		return generateStylerule((Stylerule) root.getChildren().get(0));
+		StringBuilder sb = new StringBuilder();
+		for (ASTNode child : root.getChildren()) {
+			if (child instanceof Stylerule) {
+				sb.append(generateStylerule((Stylerule) child));
+			}
+		}
+		return sb.toString();
 	}
 
 	private String generateStylerule(Stylerule stylerule) {
 		String result = stylerule.selectors.get(0) + " {\n";
 		result += "\t" + generateDeclaration(stylerule.body.get(0));
-		result += "\n}";
+		result += "\n}\n";
 		return result;
 	}
 
 	private String generateDeclaration(ASTNode astNode) {
-		return null;
+		Declaration	declaration = (Declaration) astNode;
+		String result = declaration.property.name + ": ";
+		if(declaration.expression instanceof ColorLiteral) {
+			ColorLiteral colorLiteral = (ColorLiteral) declaration.expression;
+			result += colorLiteral.value;
+		} else if (declaration.expression instanceof ScalarLiteral) {
+			ScalarLiteral scalarLiteral = (ScalarLiteral) declaration.expression;
+			result += scalarLiteral.value;
+		} else if (declaration.expression instanceof PercentageLiteral) {
+			PercentageLiteral percentageLiteral = (PercentageLiteral) declaration.expression;
+			result += percentageLiteral.value;
+		} else if (declaration.expression instanceof PixelLiteral) {
+			PixelLiteral pixelLiteral = (PixelLiteral) declaration.expression;
+			result += pixelLiteral.value;
+		} else if (declaration.expression instanceof BoolLiteral) {
+			BoolLiteral boolLiteral = (BoolLiteral) declaration.expression;
+			result += boolLiteral.value;
+		}
+
+		return result;
 	}
-	
 }
